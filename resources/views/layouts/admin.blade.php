c818b0058f1236990137ebad01f98b559208942f
Maina <mainaedwin913@gmail.com>
2025-02-09 20:24:16 +0300

    Update: Services Delete

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,9 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+
     @yield('styles')
 </head>
 
@@ -107,6 +109,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script>
         $(function() {
--- resources/views/partials/datatablesActions.blade.php
+++ resources/views/partials/datatablesActions.blade.php
@@ -8,10 +8,21 @@
         {{ trans('global.edit') }}
     </a>
 @endcan
-@can($deleteGate)
-    <form action="{{ route('admin.' . $crudRoutePart . '.destroy', $row->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+{{-- @can($deleteGate)
+    <form action="{{ route('admin.' . $crudRoutePart . '.destroy', $row->id) }}" method="POST" 
+        onsubmit="return confirm('{{ trans('global.areYouSure') }}');" 
+    style="display: inline-block;">
         <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+    </form>
+@endcan --}}
+@can($deleteGate)
+    <form action="{{ route('admin.' . $crudRoutePart . '.destroy', $row->id) }}" method="POST" class="delete-form" style="display: inline-block;">
+        @csrf
+        @method('DELETE')
+        <button type="button" class="btn btn-xs btn-danger delete-btn">
+            {{ trans('global.delete') }}
+        </button>
     </form>
 @endcan
